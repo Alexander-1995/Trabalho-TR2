e8af3fbd295d8f603ae38bf117b0c69808e22b72
Matheus <matheusov@gmail.com>
2017-12-03 21:52:45 -0200

    Início do proxy

---
+++ proxy.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+
+	int BUFFER_SIZE = 4096;
+
+	int server_socket;
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9874);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+	listen(server_socket, 5);
+
+	int client_socket;
+	struct sockaddr_in client_address;
+	socklen_t addrlen = sizeof(client_address);
+
+	while (1)
+	{
+		client_socket = accept(server_socket, (struct sockaddr*) &client_address, &addrlen);
+
+		char client_ip[addrlen];
+		inet_ntop(AF_INET, &client_address, client_ip, addrlen);
+		printf("Client IP: %s\n", client_ip);
+
+		char client_message[1024];
+		char client_message_copy[1024];
+
+		recv(client_socket, client_message, BUFFER_SIZE , 0);
+		printf("Client Request:\n%s\n", client_message);
+
+		char * pch;
+		char * method;
+		char * hostname;
+
+		strcpy(client_message_copy, client_message);
+
+		pch = strtok(client_message_copy, " :\n\r");
+		while (pch != NULL)
+		{
+			if (strcmp(pch, "GET") == 0) {
+				method = malloc(strlen(pch) + 1);
+				strcpy(method, pch);
+			}
+
+			if (strcmp(pch, "Host:") == 0) {
+				hostname = malloc(strlen(pch));
+				pch = strtok (NULL, " :\n\r");
+				strcpy(hostname, pch);
+				break;
+			}
+
+			pch = strtok (NULL, " \n");
+		}
+
+		printf ("Method: %s\n", method);
+		printf ("Hostname: %s\n", hostname);
+
+		struct hostent *he = gethostbyname(hostname);
+
+		if (he == NULL) {
+			printf("Erro ao obter IP do host pelo nome\n");
+			return (0);
+		}
+
+		char ipstr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, he->h_addr_list[0], ipstr, sizeof(ipstr));
+		printf("Endereco: %s\n\n", ipstr);
+
+		int remote_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+		struct sockaddr_in remote_address;
+		remote_address.sin_family = AF_INET;
+		remote_address.sin_port = htons(80);
+		inet_aton(ipstr, &remote_address.sin_addr.s_addr);
+
+		if (connect(remote_socket, (struct sockaddr *) &remote_address, sizeof(remote_address)) < 0) {
+			printf("Erro ao conectar ao socket remoto\n");
+		}
+
+		send(remote_socket, client_message, sizeof(client_message), 0);
+
+		ssize_t n;
+		char response[BUFFER_SIZE];
+
+		while ((n = recv(remote_socket, response, BUFFER_SIZE, 0)) > 0) {
+			send(client_socket, response, n, 0);
+		}
+
+		/*recv(remote_socket, &response, sizeof(response), 0);*/
+		/*printf("Remote response:\n%s\n\n", response);*/
+		/*send(client_socket, response, sizeof(response), 0);*/
+
+	}
+
+	close(server_socket);
+
+	return 0;
+}
